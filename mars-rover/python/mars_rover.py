453ee560f221a465ad010c51549a624e730a17e5
Neefs, Jef <jef.neefs@barco.com>
2019-05-10 23:58:24 +0200

    adding the rover class

--- mars-rover/python/mars_rover.py
+++ mars-rover/python/mars_rover.py
@@ -1,1 +1,6 @@
 title = "mars_rover"
+
+
+class Rover(object):
+    def __init__(self, x, y):
+        pass
--- mars-rover/python/test_mars_rover.py
+++ mars-rover/python/test_mars_rover.py
@@ -1,9 +1,12 @@
 from unittest import TestCase
 
-from .mars_rover import title
+from .mars_rover import title, Rover
 
 
 class TestMarsRover(TestCase):
     def test_the_title(self):
         """this test can be removed"""
         self.assertIn("mars", title)
+
+    def test_mars_rover_moves_north_when_facing_north_and_asked_to_go_forward(self):
+        rover = Rover(0, 0)

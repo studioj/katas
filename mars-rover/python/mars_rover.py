6bb69194386150cb0878e7ccb937d0f69241c7f9
Neefs, Jef <jef.neefs@barco.com>
2019-05-11 00:25:12 +0200

    storing location

--- mars-rover/python/mars_rover.py
+++ mars-rover/python/mars_rover.py
@@ -3,7 +3,10 @@ title = "mars_rover"
 
 class Rover(object):
     def __init__(self, direction, x, y):
-        self.location = (x, y)
+        self.__x = x
+        self.__y = y
+        self.location = (self.__x, self.__y)
 
     def forward(self):
+        self.__y += 1
         self.location = (0, 1)
